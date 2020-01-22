49989f0ee0f03d4757f39bd5610c51fc9c286b0c
Clément <clementbousquet@live.fr>
2020-01-22 19:34:28 +0100

    Valeurs par défauts

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
 from PySide2 import QtWidgets
+import currency_converter
 
 class App(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.c = currency_converter.CurrencyConverter()
         self.setWindowTitle("Convertisseur de devises")
         self.setup_ui()
+        self.set_default_values()
 
     def setup_ui(self):
         self.layout = QtWidgets.QHBoxLayout(self)
@@ -18,6 +21,19 @@ class App(QtWidgets.QWidget):
         self.layout.addWidget(self.combo_devisesTo)
         self.layout.addWidget(self.spin_montantConv)
         self.layout.addWidget(self.button_invert)
+
+
+    def set_default_values(self):
+        self.combo_devisesFrom.addItems(sorted(list(self.c.currencies)))
+        self.combo_devisesTo.addItems(sorted(list(self.c.currencies)))
+        self.combo_devisesFrom.setCurrentText("EUR")
+        self.combo_devisesTo.setCurrentText("EUR")
+
+        self.spin_montant.setRange(1, 1000000000)
+        self.spin_montantConv.setRange(1, 1000000000)
+        self.spin_montant.setValue(100)
+        self.spin_montantConv.setValue(100)
+
 
 app = QtWidgets.QApplication([])
 win = App()
